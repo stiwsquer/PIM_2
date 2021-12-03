e66071b5f4e4a2ad5ab1ab109c4eaddcd1f16187
ty7ak <k3enan404@gmail.com>
2021-12-03 16:23:40 +0100

    Added rate screen and local storage

--- screens/CreateNoteScreen.js
+++ screens/CreateNoteScreen.js
@@ -1,14 +1,91 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, FAB } from 'react-native-elements';
+import StarRating from 'react-native-star-rating-widget';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function CreateNoteScreen({ route }) {
+export default function CreateNoteScreen({ route, navigation }) {
+  const [rating, setRating] = useState(0);
+  const [desc, setDesc] = useState(''); 
+  
   const { item } = route.params;
+
+  let note = {
+    beerID: item.id,
+    beerName: item.name,
+    beerRating: rating,
+    beerDescription: desc
+  }
+
+  const saveNote = async () => {
+    try {
+      await AsyncStorage.setItem(item.id, JSON.stringify(note)).then(() => navigation.navigate("Saved"))
+
+    } catch (e) {
+      // handle error
+    }    
+  }
+
   return (
-    <View>
-      <Text>Form goes here</Text>
-      <Text>Beer id: {item.id}</Text>
-      <Text>Beer name: {item.name}</Text>
+    <View style={styles.container}>
+      <Text style={styles.beerName}>{item.name}</Text>
+      <Text style={styles.rateText}>Rate your beer</Text>
+      <StarRating
+        style={styles.rating}
+        rating={rating}
+        starSize={50}
+        enableSwiping={true}
+        onChange={setRating}
+      />
+      <Input
+        style={styles.input}
+        placeholder='Add your description'
+        multiline={true}
+        maxLength={200}
+        blurOnSubmit={true}
+        keyboardType="default"
+        returnKeyType="done"
+        value ={desc}
+        onChangeText={desc => setDesc(desc)}
+      />
+      <Text style={{fontSize: 20, textAlign: 'center'}}> tutaj geolokacja </Text>
+      <FAB
+        style={styles.submitButton}
+        icon={{ name: 'done', color: 'white'}}
+        onPress={() => saveNote()}
+      />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  beerName: {
+    textAlign: 'center',
+    fontSize: 52,
+    margin: 10
+  },
+  rateText: {
+    fontSize: 32,
+  },
+  rating: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingBottom: 20
+  },
+  input: {
+    minHeight: 20,
+    maxHeight: 100,
+    paddingTop: 20
+  },
+  submitButton: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
+
--- screens/SavedNotes.js
+++ screens/SavedNotes.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Button, View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/core';
 
 export default function SavedNotes({ navigation }) {
+  
+  const [notes, setNotes] = useState([])
+
+  useFocusEffect(
+    React.useCallback(() => {
+      readNotes()
+    }, [])
+  )
+
+  const readNotes = async () => {
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      const notes = await AsyncStorage.multiGet(keys);
+
+      setNotes(notes)
+    } catch(e) {
+      // Handle error
+    }
+  }
+
   return (
+    
     <View style={styles.container}>
-      <Text>Saved notes</Text>
+      <Text>{notes}</Text>
+      <Button
+        title="DELETE ALL NODES (DEV)"
+        onPress={() => AsyncStorage.clear().then(() => readNotes()) }
+      />
       <Button
         title="Create note"
         onPress={() => navigation.navigate('SelectBeer')}
