42b3cc8bb5bca2b2d0773ea8f4e49d09437fc9ca
Dawid Dopart <dawid.dopart@gmail.com>
2021-12-11 19:33:01 +0100

    Basic styling details screen

--- screens/NoteDetails.js
+++ screens/NoteDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button, View, Text} from 'react-native';
+import { Button, Image, View, Text, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import StarRating from 'react-native-star-rating-widget';
 
 export default function NoteDetails({ navigation, route }) {
-  
+
   let note = {
     beerID: route.params.beerID,
     beerName: route.params.beerName,
@@ -14,7 +15,7 @@ export default function NoteDetails({ navigation, route }) {
   const removeNote = async () => {
     try {
       await AsyncStorage.removeItem(route.params.beerID)
-    } catch(e) {
+    } catch (e) {
       // Handle error
     }
   };
@@ -28,13 +29,63 @@ export default function NoteDetails({ navigation, route }) {
           navigation.navigate('Saved');
         }}
       />
-      <Text>{route.params.beerName}</Text>
-      <Text>{route.params.beerRating}</Text>
-      <Text>{route.params.beerDescription}</Text>  
-      <Text>Zdjecie</Text>
-      <Text>Lokalizacja</Text>  
+      <View style={styles.main}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://engineering.fb.com/wp-content/uploads/2016/04/yearinreview.jpg',
+          }}
+        />
+        <View style={styles.inner}>
+          <Text style={styles.title}>{route.params.beerName}</Text>
+          <StarRating
+          style={styles.rating}
+          rating={route.params.beerRating}
+          enableSwiping="false"
+          disabled="true"
+          onChange={() => { }}
+          />
+        </View>
+      </View>
+      <View>
+        <Text style={styles.desc}>{route.params.beerDescription}</Text>
+        <Text>Lokalizacja</Text>
+
+      </View>
 
     </View>
   );
 }
 
+
+const styles = StyleSheet.create({
+  rating: {
+    marginLeft: 25,
+    marginTop: 35,
+    alignSelf: 'flex-start'
+  },
+  image: {
+    width: 125,
+    height: 150,
+    margin: 10,
+    resizeMode: 'stretch',
+  },
+  title: {
+    margin: 5,
+    fontSize: 32,
+    alignSelf: 'center',
+  },
+  desc: {
+    fontSize: 22,
+    marginVertical: 10,
+    marginHorizontal: 20,
+  },
+  main: {
+    flexDirection: 'row',
+    borderBottomWidth: 2,
+  },
+  inner: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+});
